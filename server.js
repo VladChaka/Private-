df766fbd47adc4382c8f5bf82e32244f548389fe
Vlad <super.chuhunov@yandex.com>
2018-07-23 15:57:53 +0300

    Fix my fails

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var express      = require('express');
-var app          = express();
-var bodyParser   = require('body-parser');
-var mongoose     = require('mongoose');
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var cookieParser = require('cookie-parser');
 
 mongoose.connect('mongodb://darkmars:weblanser228@ds219100.mlab.com:19100/cookies', { useNewUrlParser: true }, function(err) {
@@ -11,30 +11,9 @@ mongoose.connect('mongodb://darkmars:weblanser228@ds219100.mlab.com:19100/cookie
     });
 });
 
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text({ type: 'text/html' }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(express.static(__dirname + '/public'));
 
 app.use('/api', require('./app/controllers/index'));
-
-app.use(function(req, res, next) {
-    res.status(404);
-    console.log(`Not found URL: ${req.url}`);
-    res.send({ error: 'Not found' });
-    next();
-});
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    console.log(`Internal error(${res.statusCode}): ${err.message}`);
-    res.send({ error: err.message });
-    return;
-});
